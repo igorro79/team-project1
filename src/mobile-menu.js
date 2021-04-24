2e11e308d4e18f2b853e8069de7e62702e9bc3f5
Roman Kruk <wilmlass@gmail.com>
2021-04-24 12:56:48 +0300

    reworked js for mobile menu

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,14 +1,20 @@
 const openMobileMenuBtn = document.querySelector('.burger__btn')
 const closeMobileMenuBtn = document.querySelector('.close__btn')
+const heroBackdrop = document.querySelector('.header__backdrop')
 const menu = document.querySelector('.page-header')
+const navLinks = document.querySelectorAll('.nav__link')
 
-openMobileMenuBtn.addEventListener("click", () => {
-    menu.classList.toggle('menu-open')
-    document.body.classList.toggle('menu-open')
+openMobileMenuBtn.addEventListener("click", toggleMenu)
+closeMobileMenuBtn.addEventListener("click", toggleMenu)
+heroBackdrop.addEventListener('click', toggleMenu)
 
+navLinks.forEach((link) => {
+    link.addEventListener('click', toggleMenu)
 })
 
-closeMobileMenuBtn.addEventListener("click", () => {
+function toggleMenu() {
     menu.classList.toggle('menu-open')
     document.body.classList.toggle('menu-open')
-})
+}
+
+
